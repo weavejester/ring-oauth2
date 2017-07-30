76388ee57eaa4592422dcfed10c16b2836e14170
James Reeves <jreeves@weavejester.com>
2017-07-30 21:59:31 +0100

    Change license to MIT to match Ring

--- README.md
+++ README.md
@@ -10,5 +10,4 @@ FIXME
 
 Copyright © 2017 FIXME
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Released under the MIT License.
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ring-oauth2 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.7.1"]
                  [clj-http "3.6.1"]
