eb9c1de896e351bc6d2c8a857692c1cb86275999
James Reeves <jreeves@weavejester.com>
2017-07-31 14:19:04 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ring-oauth2 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "OAuth 2.0 client middleware for Ring"
+  :url "https://github.com/weavejester/ring-oauth2"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
