667b9b5d73759c4abdb39ddd458724c61feb1c07
James Reeves <jreeves@weavejester.com>
2017-11-12 03:20:44 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cheshire "5.7.1"]
-                 [clj-http "3.6.1"]
-                 [clj-time "0.13.0"]
-                 [ring/ring-core "1.6.2"]]
+                 [cheshire "5.8.0"]
+                 [clj-http "3.7.0"]
+                 [clj-time "0.14.0"]
+                 [ring/ring-core "1.6.3"]]
   :profiles
   {:dev {:dependencies [[clj-http-fake "1.0.3"]
                         [ring/ring-mock "0.3.1"]]}})
