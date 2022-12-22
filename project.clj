502030229ef41c23beeb0e19b02e028869156f20
James Pratt <me@jamiep.org>
2022-12-22 23:28:06 +0000

    updated dependencies - fixes #46

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/weavejester/ring-oauth2"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cheshire "5.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [cheshire "5.11.0"]
                  [clj-http "3.12.3"]
                  [clj-time "0.15.2"]
-                 [ring/ring-core "1.9.4"]]
+                 [ring/ring-core "1.9.6"]]
   :profiles
   {:dev {:dependencies [[clj-http-fake "1.0.3"]
                         [ring/ring-mock "0.4.0"]]}})
