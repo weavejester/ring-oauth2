86561338e0a8b08f6d6559d54f34545c83e0c721
James Reeves <jreeves@weavejester.com>
2024-07-06 14:13:02 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/weavejester/ring-oauth2"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cheshire "5.11.0"]
-                 [clj-http "3.12.3"]
-                 [ring/ring-core "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [cheshire "5.13.0"]
+                 [clj-http "3.13.0"]
+                 [ring/ring-core "1.12.2"]]
   :profiles
   {:dev {:dependencies [[clj-http-fake "1.0.4"]
                         [ring/ring-mock "0.4.0"]]}})
